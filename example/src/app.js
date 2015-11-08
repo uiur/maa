ed8b5d4bc53e6587c064c530d40724a73d5107e1
Kazato Sugimoto <uiureo@gmail.com>
2015-11-09 01:57:28 +0900

    Setup dev tools

---
+++ example/src/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Editor from '../../src/index.js'
+
+ReactDOM.render(<Editor />, document.getElementById('main'))
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Editor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <span>hi!</span>
+    )
+  }
+}
